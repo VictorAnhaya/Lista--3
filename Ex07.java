ffa46b284a2bddc572605e5485d7405e4e206692
Victor Anhaya <victoranhaya8@icloud.com>
2024-03-24 21:31:49 -0300

    Exercicio 7

---
+++ Ex07.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex07{
+    public static void executar() {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.print("Digite o custo de fábrica do carro: ");
+        double custoFabrica = leitor.nextDouble();
+        
+        double porcentagemDistribuidor = 0.28;
+        double porcentagemImpostos = 0.45;
+        
+        double custoConsumidor = custoFabrica + (custoFabrica * porcentagemImpostos) + (custoFabrica * porcentagemDistribuidor);
+        
+        System.out.println("O custo ao consumidor do carro é: " + custoConsumidor);
+        
+    }
+}
+
