f4646eacab9fcd5d0072200a5edabaa146eb7bac
Victor Anhaya <victoranhaya8@icloud.com>
2024-03-24 23:44:15 -0300

    Exercicio 24

--- Programa.java
+++ Programa.java
@@ -22,7 +22,8 @@ public class Programa {
         //Ex20.executar();
         //Ex21.executar();
         //Ex22.executar();
-        Ex23.executar();
+        //Ex23.executar();
+        Ex24.executar();
 
 
         
