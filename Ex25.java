3d1ca598dcc9cb81c651264e9a3f44a322f0fe50
Victor Anhaya <victoranhaya8@icloud.com>
2024-03-24 23:49:52 -0300

    Exercicio 25

--- Ex25.java
+++ Ex25.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
-public class Ex24 {
+public class Ex25 {
     public static void executar() {
         Scanner leitor = new Scanner(System.in);
         
+        System.out.println("Digite o nome do estudante: ");
+        String nomeEstudante = leitor.nextLine();
+        
+        System.out.println("Digite o número de matrícula do estudante: ");
+        String numeroMatricula = leitor.nextLine();
+        
         System.out.println("Digite as notas do estudante:");
-
         System.out.println("Nota do trabalho de laboratório (0 a 10): ");
         double notaLaboratorio = leitor.nextDouble();
 
@@ -24,8 +29,23 @@ public class Ex24 {
         
         double notaFinal = (notaLaboratorio * 2 + notaSemestral * 3 + notaExameFinal * 5) / 10;
         
-        System.out.println("A nota final do estudante é: " + notaFinal);
+        String classificacao;
+        if (notaFinal >= 8) {
+            classificacao = "A";
+        } else if (notaFinal >= 7) {
+            classificacao = "B";
+        } else if (notaFinal >= 6) {
+            classificacao = "C";
+        } else if (notaFinal >= 5) {
+            classificacao = "D";
+        } else {
+            classificacao = "R";
+        }
+        
+        System.out.println("Nome do estudante: " + nomeEstudante);
+        System.out.println("Número de matrícula: " + numeroMatricula);
+        System.out.println("Nota final: " + notaFinal);
+        System.out.println("Classificação: " + classificacao);
         
     }
 }
-
